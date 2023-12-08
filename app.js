639dfe6de3c0eb1be43eaf635da95f450aee96b2
WALKER <divakarnaidumahanthi@gmail.com>
2023-12-08 23:18:23 +0530

    deploying

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 const app = express();
-const port = 3000;
+app.use(cors());
+const port = 5000;
 dotenv.config();
 
 const mongodb_url = process.env.MONGODB_URL;
@@ -31,7 +33,7 @@ const Users = mongoose.model("User", userSchema);
 const page_limit = 20;
 
 app.get("/", async (req, res) => {
-  res.send("Hello, MongoDB and Express!");
+  res.send("Hello Worlds!");
 });
 
 app.get("/get_all_users", async (req, res) => {
@@ -45,11 +47,14 @@ app.get("/get_all_users", async (req, res) => {
   }
 });
 
-app.get("/get_one_user/:id", async (req, res) => {
-  const id = req.params.id;
+app.get("/get_searched_users", async (req, res) => {
+  const first_name = req.query.name;
   try {
     const options = { _id: 0 };
-    const users = await Users.findOne({ id: id }, options);
+    const users = await Users.find(
+      { first_name: new RegExp(first_name, "i") },
+      options
+    );
     res.json(users);
   } catch (error) {
     console.error("Error fetching users:", error);
@@ -57,11 +62,29 @@ app.get("/get_one_user/:id", async (req, res) => {
   }
 });
 
-app.get("/get_users_pagenation/:id", async (req, res) => {
-  const start = req.params.id;
+app.get("/get_users", async (req, res) => {
+  const start = req.query.start;
+  const domain = req.query.domain;
+  const gender = req.query.gender;
+  const available = req.query.available;
+  const searchBy = req.query.searchBy;
+  var searchValue = req.query.searchValue;
+  const filters = {};
+  if (domain) {
+    filters["domain"] = domain;
+  }
+  if (gender) {
+    filters["gender"] = gender;
+  }
+  if (available) {
+    filters["available"] = available == "available" ? true : false;
+  }
+  if (searchBy) {
+    filters[searchBy] = new RegExp(searchValue, "i");
+  }
   try {
     const options = { _id: 0 };
-    const users = await Users.find({}, options)
+    const users = await Users.find(filters, options)
       .skip(start - 1)
       .limit(page_limit);
     res.json(users);
@@ -71,40 +94,20 @@ app.get("/get_users_pagenation/:id", async (req, res) => {
   }
 });
 
-app.get("/get_domains", async (req, res) => {
+app.get("/get_domains_genders", async (req, res) => {
   try {
     const options = { _id: 0 };
     const users = await Users.find({}, options);
-    const s = new Set();
+    var domains = new Set();
+    var genders = new Set();
     for (const user of users) {
-      s.add(user.domain);
+      domains.add(user.domain);
+      genders.add(user.gender);
     }
-    const domains = Array.from(s);
-    res.json(domains);
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-app.get("/get_users_by_filter", async (req, res) => {
-  const domain = req.query.domain;
-  const gender = req.query.gender;
-  const available = req.query.available;
-  const filters = {};
-  if (domain) {
-    filters["domain"] = domain;
-  }
-  if (gender) {
-    filters["gender"] = gender;
-  }
-  if (available) {
-    filters["available"] = available;
-  }
-  try {
-    const options = { _id: 0 };
-    const users = await Users.find(filters, options);
-    res.json(users);
+    domains = Array.from(domains);
+    genders = Array.from(genders);
+    const data = { domains: domains, genders: genders };
+    res.json(data);
   } catch (error) {
     console.error("Error fetching users:", error);
     res.status(500).json({ error: "Internal Server Error" });
